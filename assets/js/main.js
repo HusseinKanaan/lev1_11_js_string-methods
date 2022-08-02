8f95d0a4d777956e4bcd890c52135b2823c12966
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-02 21:24:11 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let x = "Hello "
+let y = "World <br>"
+
+//Aufgabe 1
+let z = x + y;
+document.write(`${z}`);
+
+//Aufgabe 2
+let n = x + " " + y;
+document.write(n);
+
+//Aufgabe 3
+x += y;
+
+//Aufgabe 4
+document.write(x)
+
+//
+
+let meinString = "Ich bin Erster"; 
+meinString += " Ich komme auf Platz zwei"
+console.log(meinString)
+
+
